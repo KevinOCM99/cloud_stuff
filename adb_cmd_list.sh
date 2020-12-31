f2efcbe2350f85422ce0f4f017ed5fd0f0da0a92
Kevin's MacBook Pro <dbaami@hotmail.com>
2020-12-31 10:57:27 +0800

    update adb cmds

--- adb_cmd_list.sh
+++ adb_cmd_list.sh
@@ -1,12 +1,23 @@
 ##************* common commands list *************
+##** create normal **
 ##oci db autonomous-database create --profile $PV --db-name BJSCCADW0506 --cpu-core-count 1 --data-storage-size-in-tbs 1 --admin-password WelcomE__12345
+##** create free-tier **
+##oci db autonomous-database create --profile $PV --is-free-tier true --db-workload DW --admin-password WelcomE__12345 --db-name ADWCUSG --cpu-core-count 1 --data-storage-size-in-tbs 1
+##**
 ##oci db autonomous-database list --profile $PV --output table --query "data [*].{C01_ID:id, C00_DbName:\"db-name\"}"
+##**
 ##oci db autonomous-database list --profile $PV --output table --query "data [*].{C00_InsName:\"display-name\", C01_DbName:\"db-name\", C02_OCPU:\"cpu-core-count\", C03_TB:\"data-storage-size-in-tbs\", C04_Verion:\"db-version\", C05_Status:\"lifecycle-state\", C06_CreateTime:\"time-created\"}"
+##**
 ##oci db autonomous-database get --profile $PV --autonomous-database-id 
+##**
 ##oci db autonomous-database generate-wallet --profile $PV --file /tmp/myWallet.zip --password Welcome_1 --autonomous-database-id 
+##**
 ##oci db autonomous-database delete --profile $PV --autonomous-database-id 
+##**
 ##oci db autonomous-database stop --profile $PV --autonomous-database-id 
+##**
 ##oci db autonomous-database start --profile $PV --autonomous-database-id 
+##**
 alias adb_hh='echo adb_lv shows current PV, adb_ll shows less, adb_ls shows more and adb_cc to clear'
 alias adb_lv='echo current PV is [ $PV ], change it by command of [ export PV=XX ]'
 alias adb_ll='oci db autonomous-database list --profile $PV --output table --query "data [*].{C01_ID:id, C00_DbName:\"db-name\"}"'
