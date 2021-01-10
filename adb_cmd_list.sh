5b0932bc39368edaf4529006e02b8afedc9517e2
Kevin's MacBook Pro <dbaami@hotmail.com>
2021-01-10 08:21:50 +0800

    update adb-cmd path

--- adb_cmd_list.sh
+++ adb_cmd_list.sh
@@ -22,7 +22,7 @@ alias adb_hh='echo adb_lv shows current PV, adb_ll shows less, adb_ls shows more
 alias adb_lv='echo current PV is [ $PV ], change it by command of [ export PV=XX ]'
 alias adb_ll='oci db autonomous-database list --profile $PV --output table --query "data [*].{C01_ID:id, C00_DbName:\"db-name\"}"'
 alias adb_ls='oci db autonomous-database list --profile $PV --output table --query "data [*].{C00_InsName:\"display-name\", C01_DbName:\"db-name\", C02_OCPU:\"cpu-core-count\", C03_TB:\"data-storage-size-in-tbs\", C04_Verion:\"db-version\",C05_Type:\"db-workload\", C06_Status:\"lifecycle-state\", C07_CreateTime:\"time-created\"}"'
-alias adb_al='cat ./adb_cmd_list.sh | grep "##"'
+alias adb_al='cat ~/cloud_stuff/adb_cmd_list.sh | grep "##"'
 alias adb_cc='unalias adb_hh;unalias adb_lv;unalias adb_ll;unalias adb_ls;unalias adb_cc;unalias adb_al'
 echo '************* oci commands for adb ************'
 echo 'Please use adb_hh to get help of fast commands'
