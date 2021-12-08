04a0e3439850bc4c071987bbbe7ecc037f500933
Kevin's MacBook Pro <dbaami@hotmail.com>
2021-12-08 14:44:46 +0800

    myip update

---
+++ myip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo ">>>>>>>>>>>>>>begin>>>>>>>>>>>>>"
+echo
+echo "--------------------------------"
+curl cip.cc
+echo "--------------------------------"
+curl myip.ipip.net
+echo "--------------------------------"
+curl http://members.3322.org/dyndns/getip
+echo "--------------------------------"
+curl ip.sb
+echo "--------------------------------"
+curl inet-ip.info
+echo "--------------------------------"
+curl icanhazip.com
+echo "--------------------------------"
+curl ident.me
+echo
+echo "--------------------------------"
+echo
+echo ">>>>>>>>>>>>>>>end>>>>>>>>>>>>>>"
+echo
