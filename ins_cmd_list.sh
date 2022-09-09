3615d01353b4af3084f0e6d66ea0309c58e76834
KevinOCM99 <dbaami@hotmail.com>
2022-09-10 06:59:36 +0800

    update ins quick cmd

--- ins_cmd_list.sh
+++ ins_cmd_list.sh
@@ -8,8 +8,8 @@ alias ins_lv='echo current PV is [ $PV ], change it by command of [ export PV=XX
 alias ins_ll='oci compute instance list --profile $PV --output table --query "data [*].{C01_ID:id, C00_InsName:\"display-name\"}"'
 alias ins_ls='oci compute instance list --profile $PV --output table --query "data [*].{C00_InsName:\"display-name\", C01_Shape:\"shape\", C02_Region:\"region\", C03_FD:\"fault-domain\", C04_OCPU:\"ocpus\",C05_RAM:\"memory-in-gbs\", C06_Status:\"lifecycle-state\", C07_CreateTime:\"time-created\"}"'
 alias ins_al='cat ~/cloud_stuff/ins_cmd_list.sh | grep "##"'
-alias ins_cc='unalias ins_hh;unalias ins_lv;unalias ins_ll;unalias ins_ls;unalias ins_cc;unalias ins_al'
-echo -e 'oci compute instance action --action START --profile $PV --instance-id \noci compute instance action --action STOP --profile $PV --instance-id '
+alias ins_cc='unalias ins_hh;unalias ins_lv;unalias ins_ll;unalias ins_ls;unalias ins_cc;unalias ins_al;unalias ins_cmd'
+alias ins_cmd="echo -e 'oci compute instance action --action START --profile $PV --instance-id \noci compute instance action --action STOP --profile $PV --instance-id '"
 echo '************* oci commands for adb ************'
 echo 'Please use ins_hh to get help of fast commands'
 echo 'Please use ins_al to show all'
